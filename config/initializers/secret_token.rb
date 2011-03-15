1ee377e4dc7ca0f8c81ecc54a18f455a11b04e5c
Jesse Brown <jbrown@wpm145a.corp.w3data.com>
2011-03-15 12:21:29 -0700

    test rails app--first commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestJob::Application.config.secret_token = '544f20013f0a2d6bfccd3b826edb8bbc0311abf0a3786b1131ebda677ecc39ffc5763ffa1c28b635be4e8c3f25285e843d5497c9c8c99c3d50b6cd3407fc8951'
